cdc09ac6501dae0b2e015120b705bdcbf98e8ba7
Oleg Smile <valsov-9988@mail.ru>
2023-05-14 21:37:01 +0300

    Add Weapon

--- Seminar_oop_4/Main/src/Warrior.java
+++ Seminar_oop_4/Main/src/Warrior.java
@@ -1,2 +1,28 @@
-public class Warrior {
+import java.util.Random;
+
+public class Warrior{
+    private String name;
+
+    private Weapon weapon;
+
+    private int health;
+
+    public Warrior(String name, Weapon weapon, int health) {
+        this.name = name;
+        this.weapon = weapon;
+        this.health = health;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return String.format("Warrior {name = %s, weapon = %d, health = %d",name, weapon, health);
+    }
+
+    public int hit() {
+        Random random = new Random();
+        return random.nextInt(weapon.damage());
+    }
+
 }
